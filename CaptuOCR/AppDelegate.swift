9f0cb72d7efbd5ffc31ad1029abf034883e281f3
carlos andres chaguendo <c.chaguendo@mayorgafirm.com>
2021-02-24 18:28:19 -0500

    Initial Commit

---
+++ CaptuOCR/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CaptuOCR
+//
+//  Created by Carlos Andres Chaguendo Sanchez on 24/02/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
